550c02edb9adb16765e42fc6650446b18fd7425a
Mayra Luna <mayraluna9723@gmail.com>
2025-06-23 18:59:28 -0500

    Add origin desde netlify y vercel

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://ui-burguer-station.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://burguerstation.netlify.app', 'https://burguerstation.vercel.app/'],
     methods: ['GET', 'POST'],
     credentials: true,
   });
